d9f86e933d2a3125a57de87ba347bac15d348a4f
poinalex01 <poinalex01@gmail.com>
2026-08-26 22:57:42 +0200

    Revert "added GUI button to find Notepad process"
    This reverts commit 8036d50a14204b5f99cc8a3b240c98ec6d7fa16c.

--- src/gui/main.cpp
+++ src/gui/main.cpp
@@ -1,50 +1,13 @@
-#include "anticheat_guard.h"
-#include "process_finder.h"
-
 #include <QApplication>
 #include <QMainWindow>
-#include <QPushButton>
-#include <QTextEdit>
-#include <QVBoxLayout>
-#include <QWidget>
 
-int main(int argc, char *argv[])
-{
-    QApplication app(argc, argv);
+int main(int argc, char* argv[]) {
+	QApplication app(argc, argv);
 
-    QMainWindow window;
-    window.setWindowTitle("MemScope");
-    window.resize(800, 600);
+	QMainWindow window;
+	window.setWindowTitle("MemScope");
+	window.resize(800, 600);
+	window.show();
 
-    QWidget *centralWidget = new QWidget(&window);
-    QVBoxLayout *layout = new QVBoxLayout(centralWidget);
-
-    QPushButton *attachButton = new QPushButton("Attach to Notepad", centralWidget);
-    QTextEdit *outputBox = new QTextEdit(centralWidget);
-    outputBox->setReadOnly(true);
-
-    layout->addWidget(attachButton);
-    layout->addWidget(outputBox);
-
-    window.setCentralWidget(centralWidget);
-
-    QObject::connect(attachButton, &QPushButton::clicked, [outputBox]()
-                     {
-    if (memscope::IsAntiCheatProcessRunning()) {
-        outputBox->append("Anti-cheat process detected. Refusing to run.");
-        return;
-    }
-
-    auto pid = memscope::FindProcessIdByName(L"Notepad.exe");
-
-    if (!pid.has_value()) {
-        outputBox->append("Notepad not found.");
-        return;
-    }
-
-    outputBox->append(QString("Found Notepad, PID: %1").arg(pid.value())); });
-
-    window.show();
-
-    return app.exec();
+	return app.exec();
 }
